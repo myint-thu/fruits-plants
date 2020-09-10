4d4907cbacaf48dc864b220c82ea53f3ea9f9649
myint-thu <myintthu32094@gmail.com>
2020-09-10 11:58:20 +0630

    add new route

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
     //return view('welcome');
     return "Hello Laravel Project is coming";
 });
+Route::get('/testing', function ($value=''){
+	return "That is our project testing";
+});
