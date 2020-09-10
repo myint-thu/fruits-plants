f2e179b7bbe7fab3b1e769995507592319c877c9
soethu-win <soe385598@gmail.com>
2020-09-10 11:48:44 +0630

    Edit next  web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //return view('welcome');
-    return "Hello Laravel Project is coming";
+    return view('welcome');
+    ///return "Hello Laravel Project is coming";
 });
